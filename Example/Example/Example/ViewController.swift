cfaa0b69cec2cc8b511cdaa3464ffceef79a8e43
feather <feather@navercorp.com>
2022-04-19 18:27:03 +0900

    1

---
+++ Example/Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Feather on 2022/04/19.
+//
+
+import UIKit
+import SayHelloLibSPM
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        HelloMachine().say()
+    }
+
+
+}
+
---
+++ Sources/Classes/HelloMachine.swift
@@ -0,0 +1,19 @@
+//
+//  HelloMachine.swift
+//  
+//
+//  Created by Feather on 2022/04/19.
+//
+
+import Foundation
+
+public class HelloMachine {
+    public init() {
+        
+    }
+    
+    public func say() {
+        print("Hello There it`s SPM")
+    }
+    
+}
